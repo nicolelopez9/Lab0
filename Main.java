a5efae8bd72566ede9de7ac1cec50f7d620ff7e3
Nicole R. Lopez Torres <nicole.lopez9@upr.edu>
2018-02-24 12:33:35 -0400

    Added permiter feature

--- Main.java
+++ Main.java
@@ -10,5 +10,8 @@ class CircleDemo{
       //Area = PI*radius*radius
       double area = Math.PI * (radius * radius);
       System.out.println("The area of circle is: " + area);
+
+//Half area of the circle
+System.out.println("The half area of the circle is" + area/2);
    }
 }
